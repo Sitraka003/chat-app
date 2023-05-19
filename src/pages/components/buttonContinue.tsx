03f353bb8260e39770d3887cdfc323d9b03b7352
Miaritiana <hei.miaritiana@gmail.com>
2023-05-19 19:57:23 +0300

    chore: delete all unused files

--- src/pages/components/buttonContinue.tsx
+++
@@ -1,9 +0,0 @@
-const ButtonContinue = () => {
-    return(
-        <div className='mt-3 p-1 rounded-lg border-2 text-center text-white bg-yellow-500 border-yellow-500 cursor-pointer'>
-            <p>Continue</p> 
-        </div>
-    )
-}
-
-export default ButtonContinue
--- src/pages/components/formTitle.tsx
+++
@@ -1,9 +0,0 @@
-const FormTitle = ({title}:any) => {
-    return(
-        <div>
-            <h4 className='text-yellow-500 text-2xl text-bold text-center mb-2'>{title}</h4>
-        </div>
-    )
-}
-
-export default FormTitle
